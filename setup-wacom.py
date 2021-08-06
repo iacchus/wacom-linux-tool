771e832ba9cc1b75de817097b17a015844a206c3
I.M <=>
2021-08-06 10:08:55 -0300

    going

--- setup-wacom.py
+++ setup-wacom.py
@@ -3,11 +3,19 @@
 # https://plumbum.readthedocs.io/en/latest
 from plumbum import local
 
+XSETWACOM = local["xsetwacom"]
+
 COMMANDS = {
     "list_devices": ("--list", "devices"),
-    "get_option": lambda option: ("--get", option),
-    "set_option": lambda option, value: ("--set", option, value)
+    "get_option": lambda device, option: ("--get", device, option),
+    "set_option": lambda device, option, value: ("--set", device, option,
+                                                 value)
 }
+
+# plumbum's LocalCommand.run() -> (exit_code, stdout, stderr,)
+EXIT_CODE = 0
+STDOUT = 1
+STDERR = 2
 
 class Device:
     def __init__(self, name, id, dev_type):
@@ -17,12 +25,12 @@ class Device:
 
     def get(self, option):
         """Gets a parameter from the device's atual configuration"""
-        command = COMMANDS["get_option"](option)
+        command = COMMANDS["get_option"](self.name, option)
         return XSETWACOM.run(command)
 
     def set(self, option, value):
         """Sets a parameter to the device's atual configuration"""
-        command = COMMANDS["set_option"](option, value)
+        command = COMMANDS["set_option"](self.name, option, value)
         return XSETWACOM.run(command)
 
     def __str__(self):
@@ -36,11 +44,14 @@ def get_devices():
     devices_dict = dict()
 
     # output example:
+    #
     # >>> xsetwacom["--list", "devices"]()
-    # 'Wacom Intuos S Pad pad          \tid: 18\ttype: PAD       \nWacom Intuos S Pen stylus       \tid: 19\ttype: STYLUS    \nWacom Intuos S Pen eraser       \tid: 20\ttype: ERASER    \n'
+    # 'Wacom Intuos S Pad pad          \tid: 18\ttype: PAD       \nWacom Intuo\
+    # s S Pen stylus       \tid: 19\ttype: STYLUS    \nWacom Intuos S Pen eras\
+    # er       \tid: 20\ttype: ERASER    \n'
+    #
     command = COMMANDS["list_devices"]
-    output = XSETWACOM.run(command)
-    print(output)
+    output = XSETWACOM.run(command)[STDOUT]
 
     for device in output. \
             strip(). \
@@ -55,21 +66,15 @@ def get_devices():
 
     return devices_dict
 
-
-XSETWACOM = local["xsetwacom"]
+DEVICES = get_devices()
 
 pad = "Wacom Intuos S Pad pad"
 stylus = "Wacom Intuos S Pen stylus"
 eraser = "Wacom Intuos S Pen eraser"
 
-DEVICES = get_devices()
-
-#mode_relative = XSETWACOM["--set", stylus, "Mode", "Relative"]
-#mode_absolute = XSETWACOM["--set", stylus, "Mode", "Absolute"]
-
-DEVICES[stylus].set(option="Mode", value="Absolute")
+#DEVICES[stylus].set(option="Mode", value="Absolute")
+DEVICES[stylus].set(option="Mode", value="Relative")
 
 if __name__ == "__main__":
 
     print(get_devices())
-    pass
